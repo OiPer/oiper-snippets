33ebd93d2978de828404ea3718a36032c952b3c3
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-08-28 15:48:18 +0600

    chore: clean up test files and update .gitignore

--- packages/snippets-rs/src/lib.rs
+++ packages/snippets-rs/src/lib.rs
@@ -1,10 +1,5 @@
 mod apply_snippets;
 mod parse_config;
 
-#[cfg(test)]
-mod apply_snippets_test;
-#[cfg(test)]
-mod parse_config_test;
-
 pub use apply_snippets::apply_snippets;
 pub use parse_config::{Config, ConfigError, ConfigErrorKind, parse_config};
